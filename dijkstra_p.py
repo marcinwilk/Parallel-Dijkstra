d74e6cdc9af7dee36ef5cb7e36e17f92b544f9fc
Marcin Wilk <marcinwilk@gmail.com>
2012-03-16 00:28:23 -0500

    adding parallel tests

---
+++ dijkstra_p.py
@@ -0,0 +1,12 @@
+from dijkstra import dijkstra
+from graph import graph
+
+def dijkstra_p(g,source,p):
+    pass
+
+if __name__=="__main__":
+    p=4
+    g=graph(216).matrix
+
+    #for i in range(len(g)):
+    #    assert(dijkstra(g,i)==dijkstra_p(g,i,p))
