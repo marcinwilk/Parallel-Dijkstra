3289e769f96c3be13c154453f2a3495f76b4576b
Marcin Wilk <marcinwilk@gmail.com>
2012-03-16 22:20:25 -0500

    set up the communications needed for parallel algo

--- dijkstra_p.py
+++ dijkstra_p.py
@@ -1,12 +1,62 @@
-from dijkstra import dijkstra
-from graph import graph
+from dijkstra import dijkstra,dijkstra_all
+from graph import graph,graph_shapes
+from time import time
+from psim import PSim
+import random 
 
 def dijkstra_p(g,source,p):
-    pass
+    comm=PSim(p)
+    myRank=comm.rank
+    chunkSize=len(g)/p
+
+    result=list(g[source])
+    remaining=list(g[source])
+
+    myGraphChunk = comm.one2all_scatter(0,g)
+    localResultView=comm.one2all_scatter(0,result)
+    localRemainView= comm.one2all_scatter(0,remaining)
+
+    for q in range(len(result)):
+        lowestLocalDist=min(localRemainView)
+        lowestLocalDistIdx=localRemainView.index(lowestLocalDist)
+
+        gatheredCandidatesDist=comm.all2one_collect(0,lowestLocalDist)
+        gatheredCandidatesIdxs=comm.all2one_collect(0,lowestLocalDistIdx+myRank*chunkSize)
+        
+        processWithLowestDist=-1
+        addedNodeIdx=-1
+        lowestOverallDistance=-1
+
+        if myRank==0:
+            lowestOverallDistance=min(gatheredCandidatesDist)
+            processWithLowestDist=gatheredCandidatesDist.index(lowestOverallDistance)
+            addedNodeIdx=gatheredCandidatesIdxs[processWithLowestDist]
+
+        receivedBroadcast = \
+            comm.one2all_broadcast(0,\
+            (processWithLowestDist,addedNodeIdx,lowestOverallDistance,))
+
+        if myRank==receivedBroadcast[0]:
+            localRemainView[receivedBroadcast[1]-myRank*chunkSize]=''
+
+        #TODO: updating result/local remaining...
+             
+
+    reassembled=comm.all2one_reduce(0,localResultView)
+    if myRank==0:
+        return reassembled
+    else:
+        return []
 
 if __name__=="__main__":
-    p=4
-    g=graph(216).matrix
+    p=2
+    g=graph(8,max_edge=10000,shape=graph_shapes.RANDOM).matrix
 
-    #for i in range(len(g)):
-    #    assert(dijkstra(g,i)==dijkstra_p(g,i,p))
+    i=random.randint(0,len(g)-1)
+    expected=dijkstra(g,i)
+    timeA=time()
+    result=dijkstra_p(g,i,p)
+    #if(result!=[])
+    #    timeB=time()    
+    #    assert(expected==result)
+    #    print "Execution time: ",timeB-timeA," seconds"
